90e7c1e1370f893093a8cb3b6387e2f3d3bf359c
Aurozia <valentineoay@gmail.com>
2024-06-26 00:42:32 +0200

    fix: html + css files and added responsive

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="styles.css" />
+  <link rel="stylesheet" href="/WuWaCodex/css/reset.css" />
+  <link rel="stylesheet" href="/WuWaCodex/css/styles.css" />
   <link rel="shortcut icon" href="/WuWaCodex/pictures/image.png" type="image/x-icon" />
   <title>WuWa Codex</title>
 </head>
@@ -16,9 +17,7 @@
       <nav id="header__nav">
         <a class="button" href="#">Home</a>
         <a class="button" href="#">Characters</a>
-        <a class="button" href="#">Attributes</a>
         <a class="button" href="#">Weapons</a>
-        <a class="button" href="#">Echoes</a>
       </nav>
     </header>
 
@@ -28,18 +27,20 @@
           <img src="/WuWaCodex/pictures/image.png" alt="WuWa logo" class="character__picture" />
         </div>
         <article>
-          <section class="stars__section">
-            <?php for ($i = 0; $i < 5; $i++) : ?>
-              <img src="/WuWaCodex/pictures/star.svg" alt="Star Icon" class="stars">
-            <?php endfor ?>
-          </section>
           <section class="character__section">
-            <h2>Jiyan</h2>
-            <p>Attribute: Aero</p>
-            <p>Class: Mutant</p>
-            <p>Weapon: Broadblade</p>
-            <p>Birthplace: Huanglong</p>
-            <p>Quote: I have never regretted to brave the long night.</p>
+            <div>
+              <h2>Jiyan</h2>
+              <div class="stars__section">
+                <?php for ($i = 0; $i < 5; $i++) : ?>
+                  <img src="/WuWaCodex/pictures/star.svg" alt="Star Icon" class="stars">
+                <?php endfor ?>
+              </div>
+            </div>
+            <p><span class="strong">Attribute:</span> Aero</p>
+            <p><span class="strong">Class:</span> Mutant</p>
+            <p><span class="strong">Weapon:</span> Broadblade</p>
+            <p><span class="strong">Birthplace:</span> Huanglong</p>
+            <p><span class="strong">Quote:</span> <span>I have never regretted to brave the long night.</span></p>
           </section>
         </article>
       </section>
@@ -49,18 +50,20 @@
           <img src="/WuWaCodex/pictures/image.png" alt="WuWa logo" class="character__picture" />
         </div>
         <article>
-          <section class="stars__section">
-            <?php for ($i = 0; $i < 5; $i++) : ?>
-              <img src="/WuWaCodex/pictures/star.svg" alt="Star Icon" class="stars">
-            <?php endfor ?>
-          </section>
           <section class="character__section">
-            <h2>Verina</h2>
-            <p>Attribute: Spectro</p>
-            <p>Class: Congenital</p>
-            <p>Weapon: Rectifier</p>
-            <p>Birthplace: New Federation</p>
-            <p>Quote: Plants talk in a silent and sincere language. I can translate their talk for you, if you want.</p>
+            <div>
+              <h2>Verina</h2>
+              <div class="stars__section">
+                <?php for ($i = 0; $i < 5; $i++) : ?>
+                  <img src="/WuWaCodex/pictures/star.svg" alt="Star Icon" class="stars">
+                <?php endfor ?>
+              </div>
+            </div>
+            <p><span class="strong">Attribute:</span> Spectro</p>
+            <p><span class="strong">Class:</span> Congenital</p>
+            <p><span class="strong">Weapon:</span> Rectifier</p>
+            <p><span class="strong">Birthplace:</span> New Federation</p>
+            <p><span class="strong">Quote:</span> <span>Plants talk in a silent and sincere language. I can translate their talk for you, if you want.</span></p>
           </section>
         </article>
       </section>
@@ -70,18 +73,20 @@
           <img src="/WuWaCodex/pictures/image.png" alt="WuWa logo" class="character__picture" />
         </div>
         <article>
-          <section class="stars__section">
-            <?php for ($i = 0; $i < 5; $i++) : ?>
-              <img src="/WuWaCodex/pictures/star.svg" alt="Star Icon" class="stars">
-            <?php endfor ?>
-          </section>
           <section class="character__section">
-            <h2>Yinlin</h2>
-            <p>Attribute: Electro</p>
-            <p>Class: Congenital</p>
-            <p>Weapon: Rectifier</p>
-            <p>Birthplace: Huanglong</p>
-            <p>Quote: Name's Yinlin. As for what I do... Shhh, we don't talk about that in public.</p>
+            <div>
+              <h2>Yinlin</h2>
+              <div class="stars__section">
+                <?php for ($i = 0; $i < 5; $i++) : ?>
+                  <img src="/WuWaCodex/pictures/star.svg" alt="Star Icon" class="stars">
+                <?php endfor ?>
+              </div>
+            </div>
+            <p><span class="strong">Attribute:</span> Electro</p>
+            <p><span class="strong">Class:</span> Congenital</p>
+            <p><span class="strong">Weapon:</span> Rectifier</p>
+            <p><span class="strong">Birthplace:</span> Huanglong</p>
+            <p><span class="strong">Quote:</span> <span>Name's Yinlin. As for what I do... Shhh, we don't talk about that in public.</span></p>
           </section>
         </article>
       </section>
