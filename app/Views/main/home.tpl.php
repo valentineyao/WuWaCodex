8d0ef795ce94273ab7f85a4b6b5d8586308a0a82
Aurozia <valentineoay@gmail.com>
2024-06-26 17:41:23 +0200

    feat: updated home page

--- app/Views/main/home.tpl.php
+++ app/Views/main/home.tpl.php
@@ -1,79 +1,12 @@
 <main>
-  <section class="main__section">
-    <div class="picture">
-      <img src="<?= $absoluteUrl ?>/images/image.png" alt="WuWa logo" class="character__picture" />
-    </div>
-    <article>
-      <section class="character__section">
-        <div>
-          <h2>Jiyan</h2>
-          <div class="stars__section">
-            <?php for ($i = 0; $i < 5; $i++) : ?>
-              <img src="<?= $absoluteUrl ?>/images/star.svg" alt="Star Icon" class="stars">
-            <?php endfor ?>
-            <!-- <?php for ($i = 0; $i < 5; $i++) : ?>
-              <img src="<?= $absoluteUrl ?>/images/starLight.svg" alt="Star Light Icon" class="stars">
-            <?php endfor ?> -->
-          </div>
-        </div>
-        <p><span class="strong">Attribute:</span> Aero</p>
-        <p><span class="strong">Class:</span> Mutant</p>
-        <p><span class="strong">Weapon:</span> Broadblade</p>
-        <p><span class="strong">Birthplace:</span> Huanglong</p>
-        <p><span class="strong">Quote:</span> <span>I have never regretted to brave the long night.</span></p>
-      </section>
-    </article>
-  </section>
-
-  <section class="main__section">
-    <div class="picture">
-      <img src="<?= $absoluteUrl ?>/images/image.png" alt="WuWa logo" class="character__picture" />
-    </div>
-    <article>
-      <section class="character__section">
-        <div>
-          <h2>Verina</h2>
-          <div class="stars__section">
-            <?php for ($i = 0; $i < 5; $i++) : ?>
-              <img src="<?= $absoluteUrl ?>/images/star.svg" alt="Star Icon" class="stars">
-            <?php endfor ?>
-            <!-- <?php for ($i = 0; $i < 5; $i++) : ?>
-              <img src="<?= $absoluteUrl ?>/images/starLight.svg" alt="Star Light Icon" class="stars">
-            <?php endfor ?> -->
-          </div>
-        </div>
-        <p><span class="strong">Attribute:</span> Spectro</p>
-        <p><span class="strong">Class:</span> Congenital</p>
-        <p><span class="strong">Weapon:</span> Rectifier</p>
-        <p><span class="strong">Birthplace:</span> New Federation</p>
-        <p><span class="strong">Quote:</span> <span>Plants talk in a silent and sincere language. I can translate their talk for you, if you want.</span></p>
-      </section>
-    </article>
-  </section>
-
-  <section class="main__section">
-    <div class="picture">
-      <img src="<?= $absoluteUrl ?>/images/image.png" alt="WuWa logo" class="character__picture" />
-    </div>
-    <article>
-      <section class="character__section">
-        <div>
-          <h2>Yinlin</h2>
-          <div class="stars__section">
-            <?php for ($i = 0; $i < 5; $i++) : ?>
-              <img src="<?= $absoluteUrl ?>/images/star.svg" alt="Star Icon" class="stars">
-            <?php endfor ?>
-            <!-- <?php for ($i = 0; $i < 5; $i++) : ?>
-              <img src="<?= $absoluteUrl ?>/images/starLight.svg" alt="Star Light Icon" class="stars">
-            <?php endfor ?> -->
-          </div>
-        </div>
-        <p><span class="strong">Attribute:</span> Electro</p>
-        <p><span class="strong">Class:</span> Congenital</p>
-        <p><span class="strong">Weapon:</span> Rectifier</p>
-        <p><span class="strong">Birthplace:</span> Huanglong</p>
-        <p><span class="strong">Quote:</span> <span>Name's Yinlin. As for what I do... Shhh, we don't talk about that in public.</span></p>
-      </section>
-    </article>
-  </section>
+  <h1>Welcome to the Wuthering Waves Codex</h1>
+  <img src="<?= $absoluteUrl ?>/images/logo.png" alt="Logo Wuthering Waves" class="logo">
+  <div class="main__presentation">
+    <p><span class="strong">Wuthering Waves</span> is an action-adventure game set in a post-apocalyptic world brimming with mystery and excitement. This game offers stunning visuals, captivating gameplay, and an immersive narrative that promises to deliver an unforgettable gaming experience. As you delve into the remnants of a once-great civilization, you'll uncover secrets, forge alliances, and face formidable enemies.
+    </p>
+    <p> In this Codex, you'll find comprehensive information on the characters and weapons available in the game. Each character's class, attribute, weapon, birthplace, and unique quote are meticulously cataloged to help you understand their role and potential in your adventures. By learning these details, you can better strategize your gameplay and assemble a formidable team to tackle the challenges ahead.
+    </p>
+    <p>
+      Weapons are categorized into various types, each offering distinct combat styles and advantages. In this section, you’ll discover the different weapon types and the specific weapons that fall under each category. Dive in and explore the rich tapestry of characters and weaponry that <span class="strong">Wuthering Waves</span> has to offer.</p>
+  </div>
 </main>
